0eab55d2d2690d3bf7d3f697b854d44d25c801af
Enigmify <69073505+TERIHAX@users.noreply.github.com>
2023-09-02 21:00:58 +0100

    Update Init.js

--- Docs/Init.js
+++ Docs/Init.js
@@ -1,3 +1,25 @@
 // dont mind this shit lmao
 
-document.querySelectorAll('.css-175oi2r.r-ymttw5.r-1l7z4oj.r-2jelyo').forEach(e=>e.remove());
+document.querySelectorAll('.css-175oi2r.r-ymttw5.r-1l7z4oj.r-2jelyo').forEach(e => e.remove());
+
+document.head.appendChild(document.createElement('style').innerHTML = `
+*::-webkit-scrollbar {
+    width: 7px;
+}
+
+*::-webkit-scrollbar-track {
+	background: transparent;
+}
+
+*::-webkit-scrollbar-thumb {
+	background-color: #525252;
+	border-radius: 10px;
+	border: 0px none;
+ 	opacity: 0.4;
+  	transition: all 0.35s ease;
+}
+
+*::-webkit-scrollbar-thumb:hover {
+	opacity: 0.7;
+}
+`);
